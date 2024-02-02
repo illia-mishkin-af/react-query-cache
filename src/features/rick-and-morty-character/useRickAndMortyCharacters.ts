228d6c029951a9c1281ffe66f66d6e49ccd3532a
Illia Mishkin <illia.mishkin@softwareplant.com>
2024-02-02 10:44:39 +0100

    Add some comments

--- src/features/rick-and-morty-character/useRickAndMortyCharacters.ts
+++ src/features/rick-and-morty-character/useRickAndMortyCharacters.ts
@@ -1,4 +1,4 @@
-import { useQueries, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQueries, useQuery } from "@tanstack/react-query";
 import { useCallback, useMemo, useState } from "react";
 
 export type RickAndMortyResponse = {
@@ -6,15 +6,23 @@ export type RickAndMortyResponse = {
     name: string;
 };
 
+/**
+ * The same situtaion as in useJiraAvatars. We have array of characterIds and we want to update and keep in cache them.
+ */
 export const useRickAndMortyCharacters = (): { characters: RickAndMortyResponse[]; updateCharacterIds: (characterIds: string[]) => void } => {
     const [characterIds, setCharacterIds] = useState<string[]>([]);
+    // @Omer - This part selects all the characters, but it won't update when the characterIds change from another place
     // const characters = queryClient.getQueriesData<RickAndMortyResponse>({ queryKey: ["rick-and-morty-character"] })
     //                               .map(query => query[1])
     //                               .filter((character): character is RickAndMortyResponse => !!character);
+    // @Omer - I thought that it's possible to take first part of queryKey and I gonna select all queries that start with "rick-and-morty-character"
     const characters = useQuery({
         queryKey: ["rick-and-morty-character"],
         enabled: false
     });
+
+    // Always undefined
+    console.log(characters);
 
     useQueries({
         queries: characterIds.map(characterId => {
